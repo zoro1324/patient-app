04d2ead96b2b7807e479d108be109179e9023117
Naveen Kumar-2024-aib <naveen13524g@gmail.com>
2025-07-12 15:27:37 +0530

    Same as previous commit

--- myapp/rt_chat/urls.py
+++ myapp/rt_chat/urls.py
@@ -1,7 +1,7 @@
 
 from django.urls import path
 from django.contrib.auth.views import LoginView
-
+from . import views
 urlpatterns = [
     path('login/', LoginView.as_view(template_name='rt_chat/login.html'), name='login'),
     path('logout/',views.logout_view,name="logout"),
